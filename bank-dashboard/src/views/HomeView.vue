e70f5c05b0ab7b8c55084cf0f2174779991f286d
Dulmi Chamathka <chamathkawijesekara23@gmail.com>
2024-11-18 11:47:22 +0530

    first commit

---
+++ bank-dashboard/src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-app
+    id="inspire"
+    :style="{ background: $vuetify.theme.themes.dark.background }"
+  ></v-app>
+</template>
+
+<script>
+export default {
+  name: "HomeView",
+  components: {},
+};
+</script>
